b0ff341edd7f98ca066f018a7804272ff4a95221
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-10-24 12:04:45 +0300

    Destructiring complete. Moving on to displaying the results on the DOM

--- js/script.js
+++ js/script.js
@@ -33,11 +33,26 @@ function fetchDictionaryData(inputFieldValue)
     //Fetching data from the API
     fetch(fetchURL)
         .then(response => response.json())
-        .then(wordsData=>
+        .then(wordData=>
             {
-                wordsData.forEach(wordData => 
+                //Destructuring the fetch result to get the word, phonetic/promounciation, audio pronounciation link and the meanings array
+                let[{word, phonetic, phonetics: [{audio}], meanings}]=wordData
+
+                //Looping over the meanings array to get the part of speech, definition, synonyms and antonyms
+                meanings.forEach(meaning => 
                 {
-                    console.log(wordData)    
+                    //Destructuring the meaning object to get the part of speech, definitions array, synonyms and antonyms of the word
+                    let {partOfSpeech, definitions, synonyms, antonyms}=meaning
+
+                    //Looping over the definitions array to get the definition of the word and an example in a sentence
+                    definitions.forEach(definitions => 
+                    {
+                        //Destructuring the definitions object to get the definition and example in a sentence
+                        let {definition, example}=definitions
+                        
+                        //Passing the collected data as a paramenter to a function that will render the results to the DOM
+                        renderWordResults(word, phonetic, audio, partOfSpeech, definition, example, synonyms, antonyms)
+                    });
                 });
             })
 }
