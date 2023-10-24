4c0e45e5c63e5774dc0dc9dbb104ef4f57e524b0
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-10-24 11:17:28 +0300

    Handled the form submit event. Moving on to fetch data from the API

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
-let inputField=document.getElementById("search") //Declaring the input field as a global variable since it will be used multiple times
+//Declaring the input field as a global variable since it will be used multiple times
+let inputField=document.getElementById("search") 
 
 //Adding an event listener to the DOM so that when it is loaded, the input field is automatically focused
 document.addEventListener("DOMContentLoaded", ()=>
 {
     inputField.focus()
+})
+
+//Getting the form from the DOM and attaching a submit event to it
+const form=document.querySelector("form")
+form.addEventListener("submit", event =>
+{
+    //Prevent the default behaviour of the from
+    event.preventDefault()
+    
+    //Getting the input field avalue from the form
+    const inputFieldValue=inputField.value
+
+    //Calling the function that will fetch dictionary data based on the phrase passed on by the user
+    fetchDictionaryData(inputFieldValue)
+
+    //Clearing the form data that was submitted
+    form.reset()
 })
